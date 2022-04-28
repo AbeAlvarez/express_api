eae7308fb4206fca71e21ed1aae9cdcd32be6c1a
AbeAlvarez <adv.blvd@gmail.com>
2022-04-28 17:51:01 -0500

    Actualización API

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ app.use(express.json())
 
 const port = 3000
 
+//Endpoint
 //Routes
 //Methods HTTP: GET, POST, PUT, DELETE
-app.get('/v1/explorers', (req, res) =>{
-    console.log( `Get Launch X V1 API ${new Date()}`)
-    const explorer1 = {id:1, name:"explorer1"}
-    const explorer2 = {id:2, name:"explorer2"}
-    const explorers = [explorer1, explorer2]
+app.get('/v1/explorers/:id', (req, res) =>{
+    console.log( `Get by id Launch X V1 API ${new Date()}`)
+    const explorer1 = {id:007, name:"John Connor"}
+
     //HTTP CODE STATUS: 200
-    res.status(200).json(explorers) 
+    res.status(200).json(explorer1) 
 })
 
 app.listen(port, () => {
