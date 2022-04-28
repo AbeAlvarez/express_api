8d968532a3ae8df6361293deef65680b74cb6d55
AbeAlvarez <adv.blvd@gmail.com>
2022-04-28 18:33:37 -0500

    Actualización API

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const port = 3000
 
 app.get('/v1/explorers', (req, res) =>{
     console.log( `GET by id Launch X V1 API ${new Date()}`)
-    const explorer1 = {id:10, name:"John Connor"}
+    const explorer1 = {id:1, name:"John Connor"}
     const explorer2 = {id:8, name:"Rick Sanchez"}
     const explorers = [explorer1, explorer2]
 
@@ -20,7 +20,7 @@ app.get('/v1/explorers', (req, res) =>{
 
 app.get('/v1/explorers/:id', (req, res) =>{
     console.log( `GET by id Launch X V1 API ${new Date()}`)
-    const explorer1 = {id:10, name:"John Connor"}
+    const explorer1 = {id:1, name:"John Connor"}
 
     //HTTP CODE STATUS: 200
     res.status(200).json(explorer1) 
@@ -32,6 +32,21 @@ app.post('/v1/explorers', (req, res) =>{
     console.log(req.body)
     res.status(201).json({message:"Creado exitosamente"})
 })
+
+app.put('/v1/explorers/:id', (req, res) =>{
+    console.log(`PUT Launch X V1 API ${new Date()}`)
+    console.log(req.body)
+    console.log(req, params, id)
+
+    res.status(201).json({message:"Actualizado exitosamente"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) =>{
+    console.log(`DELETE Launch X V1 API ${new Date()}`)
+
+    res.status(201).json({message:"Se eliminó exitosamente"})
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening  on port ${port}`)
